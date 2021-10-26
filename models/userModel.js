5f2f4b952fa5fe06bd43b4764ba3fdee0965852e
Durjoy001 <durjoydey001@gmail.com>
2021-10-26 18:26:57 +0600

    implement password encryption method

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -20,8 +21,23 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function(el){
+                return el === this.password;
+            },
+            message: 'password ar not same!!!'
+        }
     }
+});
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password,8);
+
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User',userSchema);
