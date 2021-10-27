edc3680c905981d8742cf1c5365fba00dc1d6f47
Durjoy001 <durjoydey001@gmail.com>
2021-10-27 13:55:30 +0600

    implement user login functionalities

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,19 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync')
 
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 
 exports.signup = async(req,res) => {
     try{
         const newUser = await User.create(req.body);
 
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET,{
-            expiresIn: process.env.JWT_EXPIRES_IN
-        });
+        const token = signToken(newUser._id);
 
         res.status(201).json({
             status: 'sucess',
@@ -24,3 +29,25 @@ exports.signup = async(req,res) => {
         });
     }
 };
+
+exports.login = catchAsync(async(req,res,next) => {
+    const {email,password} = req.body;
+    
+    //Check if email and password exist
+    if(!email || !password) {
+        return next(new AppError('Please provide email and password!!',400));
+    }
+    //check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user || !await user.correctPassword(password,user.password)){
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    //if everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'sucess',
+        token
+    });
+});
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
+const User = require('./../models/userModel');
 
-exports.getAllUsers = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet implemented!!'
-    });
+
+exports.getAllUsers = async(req,res) =>{
+    try{
+        const users = await User.find();
+        res.status(200).json({
+            status: 'sucess',
+            data:{
+                users
+            }
+        });
+    }catch(err){
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 exports.createUser = (req,res) =>{
     res.status(500).json({
