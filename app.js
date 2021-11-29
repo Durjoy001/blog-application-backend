7ad6353463cdf4bd9bae1110aa158c67b0062df0
Durjoy001 <durjoydey001@gmail.com>
2021-11-29 21:38:42 +0600

    deployment inu

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use('/api/v1/blogs',blogRouter);
 app.use('/api/v1/users',userRouter);
 
 if(process.env.NODE_ENV ==='production'){  
-    app.use(express.static('build'));
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,'build','index.html'));
-    })
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+      });
 }
 module.exports = app; 
